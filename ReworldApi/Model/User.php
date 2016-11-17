c15993396701ab538cd1f3a73c4a7bdf161319d8
lwyj123 <443474713@qq.com>
2016-11-17 15:16:17 +0800

    change dbuser name

--- ReworldApi/Model/User.php
+++ ReworldApi/Model/User.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Library'.DIRECTORY_SEPARATOR.'medoo.php'; 
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Library'.DIRECTORY_SEPARATOR.'medoo.php'; 
 
 
 class Model_User {
